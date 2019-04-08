2778e3a4431632c501714b66a1c38014669bebba
Daniel Serkowski <dserkowski@soldevelo.com>
2019-04-08 18:49:56 +0200

    AO-156: Disabled OAUTH in superset

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -1,33 +1,36 @@
 """
 Superset config
 """
+import os
 from flask_appbuilder.security.manager import AUTH_OAUTH
 
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:p@ssw0rd@db:5432/open_lmis_reporting'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@db:5432/open_lmis_reporting'.format(
+    os.environ['POSTGRES_USER'], 
+    os.environ['POSTGRES_PASSWORD'])
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'thisISaSECRET_1234'
 
-AUTH_TYPE = AUTH_OAUTH
-
-OAUTH_PROVIDERS = [
-    {   'name': 'openlmis',
-        'icon': 'fa-sign-in',
-        'token_key':'access_token',
-        'remote_app': {
-            'consumer_key': 'superset',
-            'consumer_secret': 'changeme',
-            'request_token_params': {
-                'scope': 'read write'
-            },
-            'access_token_method': 'POST',
-            'access_token_headers': {
-                'Authorization':'Basic c3VwZXJzZXQ6Y2hhbmdlbWU='
-            },
-            'base_url': 'https://uat.openlmis.org/api/oauth',
-            'access_token_url': 'https://uat.openlmis.org/api/oauth/token?grant_type=authorization_code',
-            'authorize_url': 'https://uat.openlmis.org/api/oauth/authorize?'}
-     }
-]
+# AUTH_TYPE = AUTH_OAUTH
+# 
+# OAUTH_PROVIDERS = [
+#     {   'name': 'openlmis',
+#         'icon': 'fa-sign-in',
+#         'token_key':'access_token',
+#         'remote_app': {
+#             'consumer_key': 'superset',
+#             'consumer_secret': 'changeme',
+#             'request_token_params': {
+#                 'scope': 'read write'
+#             },
+#             'access_token_method': 'POST',
+#             'access_token_headers': {
+#                 'Authorization':'Basic c3VwZXJzZXQ6Y2hhbmdlbWU='
+#             },
+#             'base_url': 'https://uat.openlmis.org/api/oauth',
+#             'access_token_url': 'https://uat.openlmis.org/api/oauth/token?grant_type=authorization_code',
+#             'authorize_url': 'https://uat.openlmis.org/api/oauth/authorize?'}
+#      }
+# ]
 
 # The default user self registration role
 AUTH_USER_REGISTRATION_ROLE = "OLMIS Gamma"
