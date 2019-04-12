f27c150c6c61b30e788f02afd18a4073c091d4e4
Daniel Serkowski <dserkowski@soldevelo.com>
2019-04-12 15:36:18 +0200

    AO-156: Synchronized to reporting core changes

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -3,6 +3,7 @@ Superset config
 """
 import os
 from flask_appbuilder.security.manager import AUTH_OAUTH
+from superset_patchup.oauth import CustomSecurityManager
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@db:5432/open_lmis_reporting'.format(
     os.environ['POSTGRES_USER'], 
@@ -10,30 +11,32 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@db:5432/open_lmis_reporti
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'thisISaSECRET_1234'
 
-# AUTH_TYPE = AUTH_OAUTH
-# 
-# OAUTH_PROVIDERS = [
-#     {   'name': 'openlmis',
-#         'icon': 'fa-sign-in',
-#         'token_key':'access_token',
-#         'remote_app': {
-#             'consumer_key': 'superset',
-#             'consumer_secret': 'changeme',
-#             'request_token_params': {
-#                 'scope': 'read write'
-#             },
-#             'access_token_method': 'POST',
-#             'access_token_headers': {
-#                 'Authorization':'Basic c3VwZXJzZXQ6Y2hhbmdlbWU='
-#             },
-#             'base_url': 'https://uat.openlmis.org/api/oauth',
-#             'access_token_url': 'https://uat.openlmis.org/api/oauth/token?grant_type=authorization_code',
-#             'authorize_url': 'https://uat.openlmis.org/api/oauth/authorize?'}
-#      }
-# ]
+AUTH_TYPE = AUTH_OAUTH
+
+OAUTH_PROVIDERS = [
+    {   
+        'name': 'openlmis',
+        'icon': 'fa-sign-in',
+        'token_key':'access_token',
+        'remote_app': {
+            'consumer_key': 'superset',
+            'consumer_secret': 'changeme',
+            'request_token_params': {
+                'scope': 'read write'
+            },
+            'access_token_method': 'POST',
+            'access_token_headers': {
+                'Authorization':'Basic c3VwZXJzZXQ6Y2hhbmdlbWU='
+            },
+            'base_url': 'https://uat.openlmis.org/api/oauth',
+            'access_token_url': 'https://uat.openlmis.org/api/oauth/token?grant_type=authorization_code',
+            'authorize_url': 'https://uat.openlmis.org/api/oauth/authorize?'
+        }
+     }
+]
 
 # The default user self registration role
-AUTH_USER_REGISTRATION_ROLE = "OLMIS Gamma"
+AUTH_USER_REGISTRATION_ROLE = "OLMIS_Gamma"
 
 # Will allow user self registration
 AUTH_USER_REGISTRATION = True
@@ -50,5 +53,9 @@ CSV_EXPORT = {
     'encoding': 'utf-8',
 }
 
-from security import CustomSecurityManager
+# Custom security manager
 CUSTOM_SECURITY_MANAGER = CustomSecurityManager
+
+# Add custom roles
+ADD_CUSTOM_ROLES = True
+CUSTOM_ROLES = {'OLMIS_Gamma': {'all_datasource_access'}}
