907ac2f7aeec99e4f50104fa7ddca4f69a66faa7
Daniel Serkowski <dserkowski@soldevelo.com>
2019-05-24 16:09:21 +0200

    AO-279: Merged ReportingStack to core changes

--- reporting/config/services/nginx/set-authorization.sh
+++ reporting/config/services/nginx/set-authorization.sh
@@ -3,5 +3,5 @@
 : ${NGINX_BASIC_AUTH_USER:?"Need to set NGINX_BASIC_AUTH_USER"}
 : ${NGINX_BASIC_AUTH_PW:?"Need to set NGINX_BASIC_AUTH_PW"}
 
-echo -n "$NGINX_BASIC_AUTH_USER:" >> /etc/nginx/.htpasswd
+echo -n "$NGINX_BASIC_AUTH_USER:" >> /etc/nginx/.htpasswd && 
 openssl passwd -apr1 $NGINX_BASIC_AUTH_PW >> /etc/nginx/.htpasswd
