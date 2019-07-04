bf38ad250860428434ef7c56b2c00c091a634943
Daniel Serkowski <dserkowski@soldevelo.com>
2019-07-04 23:04:19 +0200

    AO-350: Used openlmisao/nginx-tls in ReportingStack

--- reporting/config/services/nginx-tls/certs_reloading.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-: ${RENEWAL_CHECK_PERIOD:?"Need to set RENEWAL_CHECK_PERIOD"}
-
-while true; do echo "$(date -Iseconds) Nginx certificates reloading..." && nginx -s reload; sleep $RENEWAL_CHECK_PERIOD; done
--- reporting/config/services/nginx-tls/wait_for_nginx_nifi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-while ! (http_code=$(curl -w %{http_code} -s -o /dev/null http://nginx-nifi) && ([ "$http_code" == "200" ] || [ "$http_code" == "301" ] || [ "$http_code" == "401" ])); do sleep 1; done
-echo "Nginx-nifi is ready"
